9c63955c961260575cb52367f7e4ae8563360614
Kingsssman <evgeniy.davydovsky@mail.ru>
2020-06-20 10:48:44 +0300

    feat; add a logic for switch and connect

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,46 @@
 class ToolTip {}
 
-class ProjectItem {}
+class ProjectItem {
+    constructor(id) {
+        this.id = id;
+        this.connectMoreInfoButton();
+        this.connectSwitchButton();
+    }
 
-class ProjectList {}
+    connectMoreInfoButton() {}
 
-class App {}
+    connectSwitchButton() {
+        const prjItemEl = document.getElementById(this.id);
+        const switchBtn = prjItemEl.querySelector('button:last-child');
+        switchBtn.addEventListener(
+            'click',
+            /**
+             * ! function
+             */
+        );
+    }
+}
 
+class ProjectList {
+    projects = [];
+
+    constructor(type) {
+        const prjItems = document.querySelectorAll(`#${type}-projects li`);
+        for (const prjItem of prjItems) {
+            this.projects.push(new ProjectItem(prjItem.id));
+        }
+    }
+
+    addProject() {}
+
+    switchProject() {}
+}
+
+class App {
+    static init() {
+        const activeProjects = new ProjectList('active');
+        const finishedProjects = new ProjectList('finished');
+    }
+}
+
+App.init();
